6c076921a7fde5dc1dca38d39faea8a4892c3c8b
anastasiaartuhova <an17_art@icloud.com>
2025-02-28 01:03:32 +0700

    readme fix add

--- src/stores/todoStore.ts
+++ src/stores/todoStore.ts
@@ -1,21 +1,22 @@
-import { defineStore } from 'pinia'; // Импорт функции defineStore из библиотеки Pinia. Эта функция используется для создания хранилища (store).
-import type { Task } from '../types/task'; //тип Task описывает структуру задачи (например, id, text, completed).
+import { defineStore } from 'pinia'; 
+import { useLocalStorage } from '../composable/useLocalStorage';
+import type { Task } from '../types/task'; 
 
-export const useTodoStore = defineStore('todo', { //Создание хранилища с именем 'todo', useTodoStore — это функция, которая возвращает экземпляр хранилища.// defineStore принимает два аргумента: имя хранилища ('todo') и объект с его настройками (state, actions, getters).
-    state: () => ({ // Функция, которая возвращает начальное состояние хранилища.
-        tasks: [] as Task[], //tasks — массив задач. Каждая задача имеет тип Task.
-        deletedTasks: [] as Task[],  // deletedTasks — массив удаленных задач.
-        filter: 'all' as 'all' | 'completed' | 'incomplete' | 'deleted', //filter — текущий фильтр для отображения задач.Изначально установлен в 'all'
+export const useTodoStore = defineStore('todo', { 
+    state: () => ({ 
+        tasks: useLocalStorage<Task[]>('tasks', []), 
+        deletedTasks: useLocalStorage<Task[]>('deletedTasks', []), 
+        filter: 'all' as 'all' | 'completed' | 'incomplete' | 'deleted', 
     }),
-    actions: { //Действия — это методы, которые изменяют состояние хранилища.
-        addTask(text: string) { //Добавляет новую задачу в массив tasks
+    actions: { 
+        addTask(text: string) { 
             this.tasks.push({ id: Date.now(), text, completed: false });
         },
         removeTask(id: number) {
-            const task = this.tasks.find(task => task.id === id); //находит задачу по id.
+            const task = this.tasks.find(task => task.id === id); 
             if (task) {
                 this.deletedTasks.push(task); 
-                this.tasks = this.tasks.filter(task => task.id !== id);  //удаляет задачу из основного массива.
+                this.tasks = this.tasks.filter(task => task.id !== id);  
             }
         },
         restoreTask(id: number) {
@@ -25,24 +26,24 @@ export const useTodoStore = defineStore('todo', { //Создание храни�
                 this.deletedTasks = this.deletedTasks.filter(task => task.id !== id); 
             }
         },
-        toggleTask(id: number) { // Переключает статус задачи (выполнена/не выполнена).
+        toggleTask(id: number) { 
             const task = this.tasks.find(task => task.id === id);
             if (task) {
-                task.completed = !task.completed; //меняет значение completed на противоположное.
+                task.completed = !task.completed; 
             }
         },
-        setFilter(filter: 'all' | 'completed' | 'incomplete' | 'deleted') { //станавливает текущий фильтр.
+        setFilter(filter: 'all' | 'completed' | 'incomplete' | 'deleted') { 
             this.filter = filter;
         },
         editTask(id: number, newTask: string) {
             const task = this.tasks.find(task => task.id === id);
             if (task) {
-                task.text = newTask; // обновляет текст задачи.
+                task.text = newTask; 
             }
         },
     },
-    getters: { // вычисляемые свойства, которые возвращают данные на основе состояния.
-        filteredTasks: (state) => { //Возвращает отфильтрованный список задач в зависимости от текущего значения filter
+    getters: { 
+        filteredTasks: (state) => { 
             switch(state.filter) {
                 case 'completed':
                     return state.tasks.filter(task => task.completed);
